75b7bf3b6670d9fb2e2b8c8269f69fbcde07b1ae
Rupali <rupali.yadav0@gmail.com>
2019-07-23 17:58:10 +0530

    changes to expenseItem more readable date and amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-
+import moment from "moment";
+import numeral from "numeral";
 const ExpenseListItem = ({dispatch,description, note, amount,id, createdAt}) =>(
     <div>
       <Link to={"/edit/"+id}><h1>{description}</h1></Link>
-      <p>{amount}</p>
-      <span>{createdAt}</span>
-      <p>{note}</p>
+      <p> 
+          <span>Rs. {numeral(amount).format('0.000')}</span> 
+          -
+          <span>{moment(createdAt).format('MMMM Do, YYYY')}</span>
+      </p>
+      <p>{note && <p>note- {note}</p>}</p>
     </div>
 );
 // no need of mapstatetoprops function coz we just want to access disptach method off the store
